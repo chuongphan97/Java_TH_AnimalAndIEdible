df7bc2dc1c7cdfbd0e3b535dfe2e935ec9773113
pgtc97 <79627755+pgtc97@users.noreply.github.com>
2021-04-07 15:23:08 +0700

    update

---
+++ Animal and IEdible/src/AbstractAndInterfaceTests.java
@@ -0,0 +1,31 @@
+import Edible.Edible;
+import Fruit.Fruit;
+import Fruit.Apple;
+import Fruit.Orange;
+import animal.Animal;
+import animal.Chicken;
+import animal.Tiger;
+
+public class AbstractAndInterfaceTests {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal :
+                animals) {
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof  Chicken) {
+                Edible edible = (Chicken) animal;
+                System.out.println(edible.howToEat());
+            }
+        }
+
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
+    }
+}
